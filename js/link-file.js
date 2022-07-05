3c4d7cd09f00348e176dd54adbe705485408c262
dwin688 <103591057+dwin688@users.noreply.github.com>
2022-07-05 10:38:08 +0700

    Add files via upload

---
+++ js/link-file.js
@@ -0,0 +1,7 @@
+//dwinæ›¿æ¢è¶…çº§ç­¾
+//ios
+let ios_download = 'https://8us88.com/?code=';
+
+//android
+// let android_download = 'https://dwinsource.oss-ap-southeast-1.aliyuncs.com/package/dwin.slk';
+let android_download = 'https://res.8us.com/8us.apk';
