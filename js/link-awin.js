18a31e0c37d152031055ec8b02026cff2c64041c
dwin688 <103591057+dwin688@users.noreply.github.com>
2022-07-15 01:05:09 +0700

    Add files via upload

---
+++ js/link-awin.js
@@ -0,0 +1,7 @@
+//ios
+//let ios_download = 'https://wwk2sm.com/lCF6L';
+let ios_download = 'https://wwk2sm.com/lCF6L';
+
+//android
+//let android_download = 'https://awin-on.oss-cn-hongkong.aliyuncs.com/game/release/apks/release-awin-0713.slk';
+let android_download = 'https://awin-on.oss-cn-hongkong.aliyuncs.com/game/release/apks/release-awin-0713.slk';
